08df03e9a3a5d34dd590d6de9d299ceb7b2d8863
Harsh <harshj7.net@gmail.com>
2024-04-22 23:54:12 +0530

    clean comment till video 5 hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,21 +3,13 @@ import resList from "../utils/mocData";
 import { useState, useEffect } from "react";
 
 const Body = () => {
-    //state varibale=normal js variable
-    //hook is normal js function
     const [listofResturants, setListofResturant] = useState(resList);
-
-    //() anar jo v pass karenge wo ek default variable value hoga. useState([Default value])
-    //const array me sirf index pe value change karenge to error nhi dega. pura array change pe error dega const value change ka.
-
-    // console.log(resList[0].info.id); //aise acess karenge nhi to undefined value dega resList.info.id se. BCS index map me hi hota
-
     return (
         <div className="body">
             <div className="filter-btn">
                 <button onClick={() => {
                     const filteredList = listofResturants.filter((res) => res.info.avgRating >= 4);
-                    setListofResturant(filteredList);  //when ever state variable changes. React will rerender that component.
+                    setListofResturant(filteredList);
                 }
                 }>
                     Top Rated Resturants
@@ -26,7 +18,7 @@ const Body = () => {
             <div className="res-container">
                 {
                     listofResturants.map((resturant) => {
-                        return <ResturantCard key={resturant.info.id} resData={resturant} /> //isme agar reslist v pass karenge to resList[index].info.id se acess karna padega
+                        return <ResturantCard key={resturant.info.id} resData={resturant} />
                     })
 
                 }
