10d6afeec13e69c536925bd41158487e9c75e95b
Harsh <harshj7.net@gmail.com>
2024-06-01 18:30:15 +0530

    fetched and render live data without moc data

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,32 @@
 import ResturantCard from "./ResturantCard";
-import resList from "../utils/mocData";
-import { useState, useEffect } from "react";
+import { useState, useEffect } from "react"; //named import
 
 const Body = () => {
-    const [listofResturants, setListofResturant] = useState(resList);
+    const [listofResturants, setListofResturant] = useState([]);
+
+    //useEffect is normal js function. It will take 2 arguments. 1 is call back fn and 2nd is dependency array.
+
+    useEffect(() => {
+        // console.log("Use Effect called.");
+        fetchData();
+    }, []);
+    //callback function of useEffect will be called once the component outside useeffect will be rendered has been rendered
+    //useEffect is triggered or called once the component is rendered. Here first Body component will be rendered then use effect wil be called.
+
+    const fetchData = async () => {
+        try {
+            const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+            const json = await data.json();
+            console.log(json);
+            console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants[0].info.name);
+            setListofResturant(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+        } catch (error) {
+            console.error('Failed to fetch api data:', error);
+        }
+    };
+
+    console.log("body is rendered..");
+
     return (
         <div className="body">
             <div className="filter-btn">
