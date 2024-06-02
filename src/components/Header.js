25eeb4d3984583c8c532c2fd70bdb661667cc750
Harsh <harshj7.net@gmail.com>
2024-06-03 02:04:22 +0530

    login and logout btn interchange

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,11 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants"; //named ixport
 const Header = () => {
+    // let btnName = "Login";
+    const [btnName, setbtnName] = useState("Login"); //once this useState variable changes, whole component outside use state will be rendered. thats why we need to use usestate inside a component
+    //after new value of state variable, whole component will be rerendered and btnName will be a new variable with updated value..
+    //only btn will be changed which is reconsilation or diff algorithm. if header will be refreshed only button will be refreashed.
+    console.log("Header Render");
     return (
         <div className="header">
             <div className="logo-container">
@@ -11,6 +17,10 @@ const Header = () => {
                     <li>About US</li>
                     <li>Contact US</li>
                     <li>Cart</li>
+                    <button className="login" onClick={() => {
+                        btnName === "Login" ? setbtnName("Logout") : setbtnName("Login");
+                        console.log(btnName);
+                    }}>{btnName}</button>
                 </ul>
             </div>
         </div>
