0c2ec936eae8421003dc88d7df469991e9a2f22d
Harsh <harshj7.net@gmail.com>
2024-06-02 16:22:46 +0530

    remove shimmercard component and modify shimmr component

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,28 +1,20 @@
-import ShimmerCard from "./ShimmerCard";
-import { useState } from "react";
 const Shimmer = () => {
-    const fetchData = async () => {
-        try {
-            const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-            const json = await data.json();
-            console.log(json);
-            // console.log(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants[0].info.name);
-            // setListofResturant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-            const resList = json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-            setresLen(resList);
-        } catch (error) {
-            console.error('Failed to fetch api data:', error);
-        }
-    };
-    fetchData();
-    const [resLen, setresLen] = useState([]);
     return (
         <div className="shimmer-container">
-            {
-                resLen.map((res) => {
-                    return <ShimmerCard />
-                })
-            }
+            <div className="shimmer-card">Card</div>
+            <div className="shimmer-card">Card</div>
+            <div className="shimmer-card">Card</div>
+            <div className="shimmer-card">Card</div>
+            <div className="shimmer-card">Card</div>
+            <div className="shimmer-card">Card</div>
+            <div className="shimmer-card">Card</div>
+            <div className="shimmer-card">Card</div>
+            <div className="shimmer-card">Card</div>
+            <div className="shimmer-card">Card</div>
+            <div className="shimmer-card">Card</div>
+            <div className="shimmer-card">Card</div>
+            <div className="shimmer-card">Card</div>
+            <div className="shimmer-card">Card</div>
         </div>
     )
 }
--- src/components/ShimmerCard.js
+++
@@ -1,6 +0,0 @@
-const ShimmerCard = () => {
-    return (
-        <div className="shimmer-card">Card</div>
-    );
-}
-export default ShimmerCard;
